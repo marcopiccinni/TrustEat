3b2ffc8b804937f2898981d8b52d88fcd24e71f4
marcopiccinni <45622021+marcopiccinni@users.noreply.github.com>
2018-12-18 10:57:55 +0100

    Emprovement for unifoming graphic

--- user/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class EditRecUser(forms.Form):
-    voto = forms.IntegerField(max_value=5, min_value=1, required=True)
-    descrizione = forms.CharField(max_length=130, required=False)
